fb52669a4dfcda20947d3dd2dc1064029bdb1381
akimkhabibullin <akim.khabibullin@outlook.com>
2025-07-11 17:14:16 +0000

    kkk

--- home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var menuBtn = document.querySelector('.menu-btn');
-  var sideMenu = document.getElementById('side-menu');
-
-  menuBtn.addEventListener('click', function() {
-    sideMenu.classList.toggle('open');
-  });
-});
